281bf263a932930de8619af4d34c5e4e868ed70a
hisekr <abhishekroshan11@gmail.com>
2025-09-16 02:00:10 +0530

    env update

--- src/app/page.js
+++ src/app/page.js
@@ -8,22 +8,24 @@ const routes = [
 
 const Homepage = () => {
   return (
-    <>
-      <main className="flex flex-col items-center bg-gray-100">
-        <h1 className="text-2xl font-bold mb-4">Mini Projects</h1>
-        <div className="grid grid-cols-2 gap-4">
-          {routes.map((route) => (
-            <Link
-              key={route.path}
-              href={route.path}
-              className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
-            >
-              {route.label}
-            </Link>
-          ))}
-        </div>
-      </main>
-    </>
+    <main className="min-h-screen flex flex-col items-center bg-gray-100 relative gap-4">
+      <div className="absolute top-4 left-4 text-gray-700">
+        <span className="italic font-cursive text-lg">Created by </span>
+        <span className="font-semibold">Abhishek Roshan</span>
+      </div>
+      <h1 className="text-2xl font-bold mb-4 pt-40">Mini Projects</h1>
+      <div className="grid grid-cols-2 gap-4">
+        {routes.map((route) => (
+          <Link
+            key={route.path}
+            href={route.path}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+          >
+            {route.label}
+          </Link>
+        ))}
+      </div>
+    </main>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,16 @@ import React from "react";
 import GitHubLogo from "./GithubLogo";
 
 const Navbar = ({ name, codeLink }) => {
+  const githubUrl = process.env.NEXT_PUBLIC_GITHUB_PROJECT_URL;
+  const fullUrl = githubUrl.endsWith("/")
+    ? `${githubUrl}${codeLink.slice(1)}`
+    : `${githubUrl}${codeLink}`;
+
   return (
     <div className="flex justify-between items-center px-40 py-8 bg-gray-100">
       <div className="text-lg font-semibold">{name}</div>
       <a
-        href={codeLink}
+        href={fullUrl}
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center gap-2 text-gray-700 hover:text-black transition"
